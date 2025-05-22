f228a476b73179efe3168d3df47da147183f4456
Poxygen0 <poxygen09@gmail.com>
2025-05-22 11:22:51 +0000

    chore: initialize project with basic setup

---
+++ config.py
@@ -0,0 +1,4 @@
+BOT_TOKEN = "Your_bot_token"
+DATABASE_URI = "sqlite:///database/yourdbname.db"
+ADMIN_IDS = []
+MODERATOR_IDS = []
---
+++ const.py
@@ -0,0 +1,15 @@
+HELP_MESSAGE = """
+🤖 <b>Help Menu</b>
+
+/start - Start the bot
+/help - Display a help menu with available commands
+/settings - Manage your settings
+/feedback - Send feedback directly to the admin.
+"""
+ADMIN_MENU: str = """
+🤖 <b>Admin Menu</b>
+
+/admin_commands - This menu
+/status - Check the current status of the bot
+/broadcast - broadcast message to all users
+"""
