7a3979733204eb8a57564d9b3f8bdda0b573e5b4
Poxygen0 <poxygen09@gmail.com>
2025-05-24 08:30:33 +0000

    feat(bot): add bot code to handle /start command

--- handlers/command_handlers.py
+++ handlers/command_handlers.py
@@ -1,1 +1,25 @@
-# Command Handlers 
+import asyncio
+from telegram import (Update,)
+from telegram.constants import ParseMode, ChatAction, ChatType
+from telegram.ext import ContextTypes
+from utils.decorators import *
+
+
+@send_typing_action
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    ''' Send a message when the command /start is issued.'''
+    # Gather user and chat details from the update
+    user = update.effective_user
+    chat = update.effective_chat
+    
+    user_data = {
+        "id": user.id,
+        "chat_id": chat.id,
+        "first_name": chat.first_name,
+        "last_name": chat.last_name,
+        "username": user.username,
+    }
+    welcome_message = f"👋 Hello {user_data.get('first_name', 'there')}, Welcome to the Anime News Bot! Type /help to see what I can do."
+    
+    await update.message.reply_text(welcome_message)
+    return
---
+++ main.py
@@ -0,0 +1,35 @@
+import logging
+from config import BOT_TOKEN
+from telegram import Update
+from telegram.constants import ParseMode
+from telegram.ext import (
+    ApplicationBuilder,
+    CallbackQueryHandler,
+    CommandHandler,
+    ConversationHandler,
+    Defaults,
+    filters,
+    MessageHandler,
+)
+from handlers.command_handlers import (
+    start,
+)
+# from handlers.conversation_handlers import ()
+# from handlers.message_handlers import ()
+
+
+def start_bot() -> None:
+    defaults = Defaults(parse_mode=ParseMode.HTML)
+    app = (ApplicationBuilder()
+           .token(BOT_TOKEN)
+           .defaults(defaults)
+           .build())
+    
+    
+    app.add_handler(CommandHandler('start', start))
+    
+    app.run_polling(allowed_updates=Update.ALL_TYPES, timeout=60)
+
+
+if __name__ == "__main__":
+    start_bot()
