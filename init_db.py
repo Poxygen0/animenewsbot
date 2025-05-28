1fc4c89cb8a3b350b7176e54c860e04cc680f3fd
Poxygen0 <poxygen09@gmail.com>
2025-05-28 13:17:20 +0000

    chore: setup logging and add the NewsCache table

--- init_db.py
+++ init_db.py
@@ -1,10 +1,14 @@
 # init_db.py
 from models.database import Base, db
 from models.user import User, SubscriptionLog, FavoriteAnime
+from models.news import NewsCache
+from utils.logger import setup_logger
+
+logger = setup_logger(__name__, "./data/logs/models.log")
 
 def main() -> None:
     Base.metadata.create_all(bind=db)
-    print("✅ Database initialized.")
+    logger.info("✅ Database initialized.")
     return
 
 
