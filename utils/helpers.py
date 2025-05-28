0c454cb95fdf4ed179debd4f1d7d72fcf7911dab
Poxygen0 <poxygen09@gmail.com>
2025-05-28 10:32:12 +0000

    feat(utils): added function for extracting news article

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,6 +1,8 @@
 import requests
+from bs4 import BeautifulSoup
 from models.database import SessionLocal
 from utils.logger import setup_logger
+from typing import List, Dict, Optional
 import time
 
 logger = setup_logger(__name__, "./data/logs/utils.log")
@@ -26,3 +28,29 @@ def fetch_news_page(url, retries=30, delay=5) -> str | None:
             else:
                 logger.exception(f"All {retries} attempts failed for: {url}")
                 return
+
+def extract_news_articles(page_html: str) -> List[Dict[str, Optional[str]]]:
+    ''' For parsing and extracting the news content '''
+    soup = BeautifulSoup(page_html, 'lxml')
+    articles = []
+
+    for article in soup.find_all('div', attrs={'class':'news-unit'}):
+        title_tag = article.find('p', class_='title')
+        if not title_tag:
+            continue
+        title = title_tag.get_text(strip=True)
+        link = title_tag.find('a')['href']
+        summary = article.find('div', class_='text').get_text(strip=True) if article.find('div', class_='text') else ''
+        date_str = article.find('p', class_='info').next_element.get_text(strip=True,)[:-2] if article.find('p', class_='info') else ''
+        # date_str_text = date_str.find(string=True, recursive=False)
+        image_url_tag = article.find('a', class_='image-link') if article.find('a', class_='image-link') else None
+        image_url = image_url_tag.find('img')['srcset'] if image_url_tag else None
+
+        articles.append({
+            'title': title,
+            'summary': summary,
+            'link': link,
+            'date': date_str,
+            'image_url': image_url
+        })
+    return articles
