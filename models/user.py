8f7f74cf60e78ea2a8d5db6bd033855fe2025def
Poxygen0 <poxygen09@gmail.com>
2025-06-04 23:28:38 +0000

    feat(model): added a class method for logging news subscription

--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
 from typing import Optional
 from sqlalchemy import create_engine, ForeignKey, DateTime, String, JSON
-from sqlalchemy.orm import mapped_column, declarative_base, relationship, sessionmaker, Mapped
+from sqlalchemy.orm import mapped_column, declarative_base, relationship, sessionmaker, Mapped, Session
 from sqlalchemy.sql import func
-from .database import Base
+from .database import Base, SessionLocal
 from datetime import datetime
 
 
@@ -39,8 +39,15 @@ class SubscriptionLog(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     tg_user_id: Mapped[int] = mapped_column(index=True)
-    action: Mapped[str] = mapped_column(String)
+    action: Mapped[str] = mapped_column(String) # "subscribed", "unsubscribed"
     timestamp: Mapped[Optional[datetime]] = mapped_column(DateTime, default=func.now())
+    
+    @classmethod
+    def log(cls, session:Session, tg_user_id, action) -> None:
+        log = cls(tg_user_id=tg_user_id, action=action)
+        session.add(log)
+        session.commit()
+        return
 
 
 class FavoriteAnime(Base):
