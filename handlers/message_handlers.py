e69dbdc61b5c8bb5f9a7ec9fa91ff946aaa2cc85
Poxygen0 <poxygen09@gmail.com>
2025-11-25 18:58:29 +0000

    chore: wipe existing codebase and start fresh

--- handlers/message_handlers.py
+++
@@ -1,3 +0,0 @@
-from utils.logger import setup_logger
-
-logger = setup_logger(__name__, "./data/logs/handlers.log")
--- models/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-from config import DATABASE_URI, DEBUG
-
-Base = declarative_base()
-db = create_engine(DATABASE_URI, echo=DEBUG, future=True)
-SessionLocal = sessionmaker(bind=db, autoflush=False)
