9bb414dcff6b0912f3aac4d595f7948d86cf5fc0
Poxygen0 <poxygen09@gmail.com>
2025-12-11 05:15:34 +0000

    feat(error): Add a global error handler function

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ from config import config
 # from handlers.command_handlers import ()
 # from handlers.conversation_handlers import ()
 # from handlers.message_handlers import ()
+from utils.helpers import send_critical_alert
 from utils.logger import setup_logger
 
 filterwarnings(action="ignore", message=r".*CallbackQueryHandler", category=PTBUserWarning)
@@ -30,3 +31,34 @@ filterwarnings(action="ignore", message=r".*CallbackQueryHandler", category=PTBU
 START_TIME = time.time()
 
 logger = setup_logger(__name__, config.paths.log_path+"main.log")
+
+
+# ---------------------------
+# GLOBAL ERROR HANDLER
+# ---------------------------
+async def error_handler(update: Update, context: CallbackContext):
+    """
+    Global error handler for unhandled exceptions in the bot.
+    Logs the error details, including the stack trace.
+    """
+    global ERROR_COUNT_24H
+    ERROR_COUNT_24H += 1
+    context.bot_data['error_count_24h'] = ERROR_COUNT_24H
+    # Log the error details
+    logger.exception(f"A general error occurred: {context.error}", exc_info=True)
+
+    # If the error is related to a specific update, log the update details as well
+    if update:
+        logger.exception(f"Update: {update.to_dict()}")
+
+    # Optionally, notify the user or the admin about the error (for debugging purposes)
+    if update:
+        if update.message:
+            await update.message.reply_text("Sorry, an error occurred. Please try again.")
+        elif update.callback_query:
+            await update.callback_query.edit_message_text("Sorry, an error occurred. Please try again.")
+        await send_critical_alert(
+            context, title="GLOBAL ERROR",
+            context_data={"user": update.effective_user.id if update.effective_user else None},
+            exc=context.error
+        )
