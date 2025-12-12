df6aa72def447f5b2fd02f52d1f04f21d7eb93bc
Poxygen0 <poxygen09@gmail.com>
2025-12-12 06:22:47 +0000

    add constants

---
+++ const.py
@@ -0,0 +1,15 @@
+HELP_MESSAGE = """
+🤖 <b>Help Menu</b>
+
+/start - Start the bot
+/help - Display this menu
+/add_channel - Add a channel to the db
+/mychannels - edit your channels
+/latest - fetch the latest news
+/subscribe - get scheduled news update
+/unsubscribe - stop scheduled news update
+/togglenotifications [on/off] - Mute or unmute notifications
+/feedback - Send feedback directly to the admin
+/cancel - Cancel current operation
+"""
+ERROR_MSG = "Oops🙊👀, seems there's an error on our side. Please try again."
